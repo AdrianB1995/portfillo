7e07c205b73bcfc21bf195b05f99ddd076eafba2
Adrian <adrianfrancisco.borrego@gmail.com>
2020-12-17 21:26:39 -0500

    - About page - Content page - Setup for backend email API

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Content from '../components/Content';
+
+import Hero from '../components/Hero';
 
 function AboutPage(props) {
-    return(
-        <p>About page works!</p>
+    return (
+        <div>
+            <Hero title={props.title} />
+
+            <Content>
+                <p>Hi there, my name is Adrian. I am currently a full stack engineer at JPMorgan Chase & Co. working with back-end microservices in a cloud infrastructure.</p>
+                <p>I have experince with modern-day development practices such as cloud, CICD, functional programming and agile with exposure to various frameworks.</p>
+                <p>I enjoy participating in mentorship and other leadership roles within my organization while always looking for opportunities to learn new skills both in and out of the office!</p>
+            </Content>
+        </div>
     );
 }
 
